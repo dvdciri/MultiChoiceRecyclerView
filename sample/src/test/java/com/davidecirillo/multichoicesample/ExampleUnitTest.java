483a6ed220df12768ac7aa68131d48e40d88b0d6
Davide Cirillo <dvd.ciri@gmail.com>
2016-06-30 10:41:04 +0100

    Test failed

--- sample/src/test/java/com/davidecirillo/multichoicesample/ExampleUnitTest.java
+++ sample/src/test/java/com/davidecirillo/multichoicesample/ExampleUnitTest.java
@@ -10,6 +10,6 @@ import static junit.framework.Assert.assertEquals;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        assertEquals(5, 2 + 2);
     }
 }
