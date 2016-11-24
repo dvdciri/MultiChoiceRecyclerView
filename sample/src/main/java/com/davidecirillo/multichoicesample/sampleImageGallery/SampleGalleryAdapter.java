2c37ea748a2eedefa71a0a42694c375a4b33295a
Davide Cirillo <dvd.ciri@gmail.com>
2016-11-24 17:06:00 +0000

    - Check style

--- sample/src/main/java/com/davidecirillo/multichoicesample/sampleImageGallery/SampleGalleryAdapter.java
+++ sample/src/main/java/com/davidecirillo/multichoicesample/sampleImageGallery/SampleGalleryAdapter.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.Toast;
